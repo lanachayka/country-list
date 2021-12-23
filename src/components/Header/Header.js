4d13bb837eb2b68e1afc9292f14a97960b93acc3
lanachayka <ruslana.chaikivska@gmail.com>
2021-12-23 10:39:33 +0200

    add Header, reset styles, import fonts and images

---
+++ src/components/Header/Header.js
@@ -0,0 +1,7 @@
+import st from './Header.module.css'
+
+export default function Header() {
+    return (<div className={st.wrapper}>
+                <p className={st.title}>Country List</p>
+            </div>)
+}
