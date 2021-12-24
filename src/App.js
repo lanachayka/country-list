0a821cea7dabf72130b715bab8edc56fa6ff9621
lanachayka <ruslana.chaikivska@gmail.com>
2021-12-24 12:17:00 +0200

    add response module, add arrow to header, update chooseCard functionality

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import './App.css';
 import Header from "./components/Header/Header";
 import ChooseCard from "./components/ChooseCard/ChooseCard";
 import CountryList from "./components/CountryList/CountryList";
+import {useState} from "react";
 
 function App() {
+  const [isCardChosen, setIsCardChosen] = useState(false);
   return (
     <div className="App">
-        <Header />
-        <ChooseCard />
+        <Header isCardChosen={isCardChosen}/>
+        <ChooseCard isCardChosen={isCardChosen}/>
         <CountryList />
     </div>
   );
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,7 +1,12 @@
 import st from './Header.module.css'
+import {useMediaQuery} from "react-responsive";
 
-export default function Header() {
+export default function Header({isCardChosen}) {
+    const isBigScreen = useMediaQuery({ query: '(min-width: 780px)' })
     return (<div className={st.wrapper}>
+                {!isBigScreen &&
+                    <p className={isCardChosen ? st.arrow : st.hidden}>&#8592;</p>
+                }
                 <p className={st.title}>Country List</p>
             </div>)
 }
